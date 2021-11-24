26e70897840dc55b8160f2b266e9aa8a86c0c5da
nikita202 <nikita@ostaplyuk.com>
2021-11-24 16:58:58 +0100

    return base64 messages

--- app.js
+++ app.js
@@ -227,7 +227,11 @@ app.post("/api/messages", async (req, res, next) => {
     if (!group) {
         return res.status(401).send("Group not found");
     }
-    return res.status(200).send({messages: group.messages});
+    let result = group.messages.toObject();
+    for (let e of result) {
+        e.content = Buffer.from(e.content.data).toString("base64");
+    }
+    return res.status(200).send({messages: result});
 });
 
 app.post("/api/user", async (req, res, next) => {
